0fe45c6379444953c6e570608a39315c84f21364
Furuikeya <furuburi@gmail.com>
2022-04-19 21:37:21 +0300

    creating errorbranch for finding out the truth about the error in saga put

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,11 +1,20 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { userReducer } from "./user/user.reducer";
 import { categoriesReducer } from "./categories/category.reducer";
 import { cartReducer } from "./cart/cart.reducer";
 
+// blacklisting dropdown from persist
+const cartPersistConfig = {
+  key: "cart",
+  storage: storage,
+  blacklist: ["isCartOpen"],
+};
+
 export const rootReducer = combineReducers({
   user: userReducer,
   categories: categoriesReducer,
-  cart: cartReducer,
+  cart: persistReducer(cartPersistConfig, cartReducer),
 });
